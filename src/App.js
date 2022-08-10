7d97562d9ea64e90a5e0279c690038a5521e2b85
ayoub_jnrjuol <ademzaibi@gmail.com>
2022-08-10 16:44:10 +0200

    thirdapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/navbar/Navbar';
+import FormDisabledInputExample from './component/form/FormDisabledInputExample';
+import Main from './component/main/Main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
+      <Navbar/>
+      <FormDisabledInputExample/>
+      <Main/>
+     
     </div>
   );
 }
